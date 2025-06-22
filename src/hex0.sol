0bbe9855e9b708b8c04cec7726ce40f05d5d8157
Julie B. <no-reply@bbjubjub.fr>
2025-06-22 22:33:12 +0200

    refactor

--- src/hex0.sol
+++ src/hex0.sol
@@ -14,34 +14,32 @@ contract hex0 {
         uint256 hold = 0;
         
         while (i < input.length) {
-            uint8 c = uint8(input[i]);
-            uint256 v = uint256(c);
+            uint8 v = uint8(input[i]);
             
             if (v >= 97 && v <= 102) { // 'a' to 'f'
                 hold |= v - 87;
+            } else if (v >= 48 && v <= 57) { // '0' to '9'
+                hold |= v - 48;
+            } else if (v >= 65 && v <= 70) { // 'A' to 'F'
+                hold |= v - 55;
             } else if (v == 59 || v == 35) { // ';' or '#' - comment handling
                 // Skip to end of line
                 while (true) {
                     i++;
-                    if (i >= input.length) break;
-                    uint8 nextChar = uint8(input[i]);
-                    if (nextChar == 13 || nextChar == 10) { // '\r' or '\n'
+                    v = uint8(input[i]);
+                    if (v == 13 || v == 10) { // '\r' or '\n'
                         break;
                     }
                 }
                 i++;
                 continue; // no nibble to output
-            } else if (v >= 48 && v <= 57) { // '0' to '9'
-                hold |= v - 48;
-            } else if (v >= 65 && v <= 70) { // 'A' to 'F'
-                hold |= v - 55;
             } else {
                 i++;
                 continue; // no nibble to output
             }
             
             if (toggle) {
-                output = abi.encodePacked(output, uint8(hold));
+                output = bytes.concat(output, bytes1(uint8(hold)));
                 hold = 0;
                 toggle = false;
             } else {
