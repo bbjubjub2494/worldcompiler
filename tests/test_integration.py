c06fef43a89fb9b0a17c863d3fc0bab03de8faa2
Julie B. <no-reply@bbjubjub.fr>
2025-07-04 17:00:40 +0200

    wip

---
+++ tests/test_integration.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import boa
+
+def test_compile_hex0(hex2_contract):
+    build = Path("build")
+    input_str = (build/"hex0.hex2").read_text()
+    expected_output = (build/"hex0.bin").read_bytes()
+    r = boa.env.raw_call(to_address=hex2_contract, data=input_str.encode())
+    assert r.is_success
+    breakpoint()
+    assert r.output == expected_output
