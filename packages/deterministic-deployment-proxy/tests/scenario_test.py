522534e31a2111008bdcfad8823515f1f2fc2957
Julie B. <no-reply@bbjubjub.fr>
2025-07-15 18:23:40 +0200

    update example

--- packages/deterministic-deployment-proxy/tests/scenario_test.py
+++ packages/deterministic-deployment-proxy/tests/scenario_test.py
@@ -1,12 +1,21 @@
 from deterministic_deployment_proxy import proxy_deploy, proxy_predict_address
 
 
-def test_proxy_deploy():
-    # NOTE: this initcode is a bit broken and deploys an empty contract.
+def test_example():
     initcode = bytes.fromhex(
-        "608060405234801561001057600080fd5b506040516020806101008339810180604052810190808051906020019092919050505080600081905550"
+        "61"     # PUSH2
+                 # beginning of runtime code
+        "  33"   # CALLER
+        "  FF"   # SELFDESTRUCT
+                 # end of runtime code
+        "5F"     # PUSH0
+        "52"     # MSTORE
+        "60 02"  # PUSH2 2
+        "60 1E"  # PUSH2 30
+        "F3"     # RETURN
     )
     deployment_salt = b"\xa0" * 32
     deployed_address = proxy_deploy(initcode, deployment_salt)
-    assert deployed_address == "0x7184cAA7558FE9d89096d15Df5D90D96876B4E1F"
+    # per https://gnosisscan.io/tx/0x28a86133c20fdd7235debd1e2c13a70406fd8a1ac4ec45f01ea1cdaa558f466d
+    assert deployed_address == "0x10E8540B6EE2D52fE2C57D876bD830cbC9B0E366"
     assert proxy_predict_address(initcode, deployment_salt) == deployed_address
--- packages/erc7955/tests/scenario_test.py
+++ packages/erc7955/tests/scenario_test.py
@@ -1,13 +1,21 @@
 from erc7955 import factory_deploy, factory_predict_address
 
 
-def test_factory_deploy():
-    # NOTE: this initcode is a bit broken and deploys an empty contract.
+def test_example():
     initcode = bytes.fromhex(
-        "608060405234801561001057600080fd5b506040516020806101008339810180604052810190808051906020019092919050505080600081905550"
+        "61"     # PUSH2
+                 # beginning of runtime code
+        "  33"   # CALLER
+        "  FF"   # SELFDESTRUCT
+                 # end of runtime code
+        "5F"     # PUSH0
+        "52"     # MSTORE
+        "60 02"  # PUSH2 2
+        "60 1E"  # PUSH2 30
+        "F3"     # RETURN
     )
     deployment_salt = b"\xa0" * 32
     deployed_address = factory_deploy(initcode, deployment_salt)
-    # per https://sepolia.etherscan.io/tx/0x0a2bb4a3f90508ca8546ea5ec9124a48624f50b84c5fb3cdfb8c85ea462b7e53
-    assert deployed_address == "0x75eb1d1a60e4A135790E9FDaef152ee534f9b7C4"
+    # per https://gnosisscan.io/tx/0xf103fb5862ff4018f747a6920d0578266b11496a526abb12a526ee2800e0073f
+    assert deployed_address == "0x3eBe728Be9BEE0393f476a186fb5457779cAde3d"
     assert factory_predict_address(initcode, deployment_salt) == deployed_address
