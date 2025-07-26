b12bc1029e8c76ac67f3fb1b86481b2f4d6002e2
Julie B. <no-reply@bbjubjub.fr>
2025-07-26 17:27:59 +0200

    Revert "yeet dead files"
    This reverts commit 1864edff96fa823f23d63ef8f996af49fd5d0844.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+
+  outputs = inputs: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    devShells = inputs.nixpkgs.lib.genAttrs systems (system:
+      with inputs.nixpkgs.legacyPackages.${system}; {
+        default = mkShell {packages = [
+          just
+          (mescc-tools.overrideAttrs { # hex2, M1
+            patches = [
+              (fetchpatch {
+                url = "https://github.com/bbjubjub2494/mescc-tools/commit/e5658f211d1dbcbfcd173cf397ebc08fa89d150b.patch";
+                hash = "sha256-lecU4ozYH8aP/GDwJrj6HCnKaWZsaZrBKjLuu40bMaw=";
+              })
+            ];
+          })
+        ];
+      };
+      });
+    formatter =
+      inputs.nixpkgs.lib.genAttrs systems (system:
+        inputs.nixpkgs.legacyPackages.${system}.alejandra);
+  };
+}
